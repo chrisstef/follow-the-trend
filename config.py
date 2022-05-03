07bbcf9fcb26e77cf1ea9d1086d116c7a43fc959
Kai Everdream <63172663+chrisstef@users.noreply.github.com>
2022-05-04 00:45:21 +0200

    Add files via upload
    Initial Commit

---
+++ config.py
@@ -0,0 +1,23 @@
+# Our Binance credentials.
+API_KEY = 'Your Binance Api Key goes here'
+API_SECRET = 'Your Binance Api Secret goes here'
+
+# Provide pair.
+SYMBOL = "BTC/USDT"
+
+# Tokens per trade. 
+QUANTITY = 0.0
+
+# Provide timeframe to trade.
+TIMEFRAME = '1m'
+
+# Initialize the bot by setting a lookback period. ( In a 1-min strategy, LOOKBACK = 90 --> Last 90 minutes )
+LOOKBACK = 90
+
+# Supertrend parameters.
+ATR_FACTOR = 1
+PERIOD = 5
+
+
+# How many logs would you like to display on cmd screen (changes visual only).
+LOGS_DISPLAYED = 20
